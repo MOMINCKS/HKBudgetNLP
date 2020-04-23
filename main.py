3dcd1decd5eb10cad7774cd0b3340acf16700af4
MOMINCKS <63245608+MOMINCKS@users.noreply.github.com>
2020-04-23 17:50:16 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,35 @@
 import numpy as np
 import string
 import nltk
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+###
+# This text summarization aims to extract the most important sentences from the 
+# Hong Kong Financial Budget 2020-21, which has 84 sentences and 822 different words
+# (including a few kinds of punctuations), using TFDIF score. After this algorithm,
+# there will be 5 sentences left as the summary for the whole text.
+###
+
+# 0. import
 
 text = np.loadtxt('HKBudgetNLP/text.txt', encoding='utf-8', dtype='str', delimiter='\t')
 
-### text normalization ###
+# 1. removing punctuations and trimming
 
-# 1. lower case, remove punctuations and trimming
-trans = string.punctuation
-trans = trans.replace('-','')
-trans = str.maketrans('','',trans)
-for i in range(len(text)):
-      text[i] = text[i].lower()
-      text[i] = text[i].strip()
-      text[i] = text[i].translate(trans)
+def norm(text):
+      trans = string.punctuation
+      trans = trans.replace('-','') + '‘’“”'
+      trans = str.maketrans('','',trans)
+      for i in range(len(text)):
+            text[i] = text[i].strip()
+            text[i] = text[i].translate(trans)
+      return text
 
-# 2. tokenization
-tokenized = []
-for i in range(len(text)):
-      tokenized.append(nltk.word_tokenize(text[i]))
+text = norm(text)
 
-# 3. lemmatization
-def get_pos_tag(word):
+# 2. tokenization and lemmatization
+
+def get_pos_tag(word): # determine parts of speech
       tag_dict = {"J": nltk.corpus.wordnet.ADJ,
             "N": nltk.corpus.wordnet.NOUN,
             "V": nltk.corpus.wordnet.VERB,
@@ -29,14 +37,57 @@ def get_pos_tag(word):
       tag = nltk.pos_tag([word])[0][1][0]
       return tag_dict.get(tag, nltk.corpus.wordnet.NOUN)
 
-wnl = nltk.stem.WordNetLemmatizer()
+def tokenize(text):
+            tokenized = []
+            for i in range(len(text)):
+                  tokenized.append(nltk.word_tokenize(text[i]))
+            return tokenized
 
-for sents in tokenized:
-      for i in range(len(sents)):
-            sents[i] = wnl.lemmatize(sents[i], get_pos_tag(sents[i]))
+def lemmatize(text):
+      wnl = nltk.stem.WordNetLemmatizer()
+      for sents in text:
+            for words in range(len(sents)):
+                  sents[words] = wnl.lemmatize(sents[words], get_pos_tag(sents[words]))
+            sents[0] = sents[0].lower()
+      return text
 
+text = tokenize(text)
+text = lemmatize(text)
 
+# 3. tfidf vectorization
 
+def dummy(x):
+      return x
 
+def vectorize(text):
+      tfidf = TfidfVectorizer(analyzer=dummy,stop_words='english')
+      text_sparse = tfidf.fit_transform(text)
+      return text_sparse, tfidf
 
+text_sparse, tfidf = vectorize(text)
 
+# 3. text summarization
+
+def mean_mask_text(text): # whole text level
+      mean_thresold = 0.008
+      mean_score = text.mean(axis=1)
+      mean_score_mask = mean_score>=mean_thresold
+      return mean_score_mask
+
+mean_score_mask = mean_mask_text(text_sparse)
+
+def sents_filtering(text,mask):
+      output = []
+      for i in range(len(mask)):
+            if mask[i]:
+                  output.append(text[i])
+      return output
+
+def rejoin_tokens(text):
+      for sents in range(len(text)):
+            text[sents] = ' '.join(text[sents])
+      return text
+
+text = sents_filtering(text,mean_score_mask)
+text = rejoin_tokens(text)
+print(text)
